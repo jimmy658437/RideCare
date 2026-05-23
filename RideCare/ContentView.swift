6bc2c358bb801d150f9c1f3f4c9246ed4ca11ba2
leegt <jimmy658437@gmail.com>
2026-05-23 22:16:39 +0800

    Initial Commit

---
+++ RideCare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RideCare
+//
+//  Created by 114-2Workshop12 on 2026/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RideCare/RideCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideCareApp.swift
+//  RideCare
+//
+//  Created by 114-2Workshop12 on 2026/5/23.
+//
+
+import SwiftUI
+
+@main
+struct RideCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
